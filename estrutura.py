caf5b974ce77ad7c5f988b1e88ceca0827bf9982
jamillalobo <lobo.jamilla@gmail.com>
2024-05-30 19:50:46 -0300

    crud-catalogo: fix original CRUD to Pontos Turisticos

--- estrutura.py
+++ estrutura.py
@@ -3,104 +3,104 @@ import os
 from time import sleep
 
 
-class cor:
-    VERMELHO = '\033[91m'
-    VERDE = '\033[92m'
-    AMARELO = '\033[93m'
-    AZUL = '\033[94m'
-    MAGENTA = '\033[95m'
-    CIANO = '\033[96m'
-    RESET = '\033[0m'
+# class cor:
+#     VERMELHO = '\033[91m'
+#     VERDE = '\033[92m'
+#     AMARELO = '\033[93m'
+#     AZUL = '\033[94m'
+#     MAGENTA = '\033[95m'
+#     CIANO = '\033[96m'
+#     RESET = '\033[0m'
 
 
 arquivo = os.path.join(os.path.dirname(__file__), 'bd.json')
 
 
 
-def adicionar_usuario(nome, idade):
+def adicionar_ponto_turistico(nome, bairro, tipo):
     with open(arquivo, 'r') as f:
-        usuarios = json.load(f)
+        ponto_turisticos = json.load(f)
 
-    usuarios.append({'nome': nome, 'idade': idade})
+    ponto_turisticos.append({'nome': nome, 'bairro': bairro, 'tipo': tipo})
 
     with open(arquivo, 'w') as f:
-        json.dump(usuarios, f, indent=4)
-    print("😎 USUÁRIO ADICIONADO COM SUCESSO!")
+        json.dump(ponto_turisticos, f, indent=4)
+    print("😎 PONTO TURISTICO ADICIONADO COM SUCESSO!")
 
-def listar_usuarios():
+def listar_ponto_turisticos():
     with open(arquivo, 'r') as f:
-        usuarios = json.load(f)
+        ponto_turisticos = json.load(f)
 
-    if usuarios:
-        print("=" *50)
-        print("LISTA DE USUÁRIOS:")
-        print("-" *50)
-        for usuario in usuarios:
+    if ponto_turisticos:
+        print("*" *50)
+        print("LISTA DE PONTOS TURISTICOS:")
+        print("*" *50)
+        for ponto_turistico in ponto_turisticos:
             print("*" *50)
-            print(f"NOME: {usuario['nome']}, IDADE: {usuario['idade']}")
+            print(f"NOME: {ponto_turistico['nome']}, BAIRRO: {ponto_turistico['bairro']}, TIPO: {ponto_turistico['tipo']}")
             print("*" *50)
             print("=" *50)
     else:
-        print("😒 NENHUM USUÁRIO CADASTRADO.")
+        print("😒 NENHUM PONTO TURISTICO CADASTRADO.")
 
-def atualizar_usuario(nome_antigo, novo_nome, nova_idade):
+def atualizar_ponto_turistico(nome_antigo, novo_nome, novo_bairro):
     with open(arquivo, 'r') as f:
-        usuarios = json.load(f)
+        ponto_turisticos = json.load(f)
 
-    for usuario in usuarios:
-        if usuario['nome'] == nome_antigo:
-            usuario['nome'] = novo_nome
-            usuario['idade'] = nova_idade
+    for ponto_turistico in ponto_turisticos:
+        if ponto_turistico['nome'] == nome_antigo:
+            ponto_turistico['nome'] = novo_nome
+            ponto_turistico['bairro'] = novo_bairro
             break
 
     with open(arquivo, 'w') as f:
-        json.dump(usuarios, f, indent=4)
-    print("😙 USUÁRIO ATUALIZADO COM SUCESSO!")
+        json.dump(ponto_turisticos, f, indent=4)
+    print("😙 PONTO TURISTICO ATUALIZADO COM SUCESSO!")
 
-def excluir_usuario(nome):
+def excluir_ponto_turistico(nome):
     with open(arquivo, 'r') as f:
-        usuarios = json.load(f)
+        ponto_turisticos = json.load(f)
 
-    for usuario in usuarios:  
-        if usuario['nome'] == nome:
-            usuarios.remove(usuario)
+    for ponto_turistico in ponto_turisticos:  
+        if ponto_turistico['nome'] == nome:
+            ponto_turisticos.remove(ponto_turistico)
 
     with open(arquivo, 'w') as f:
-        json.dump(usuarios, f, indent=4)
-        print("😡 USUÁRIO EXCLUÍDO COM SUCESSO!")
+        json.dump(ponto_turisticos, f, indent=4)
+        print("😡 PONTO TURISTICO EXCLUÍDO COM SUCESSO!")
 
-def buscar_usuario (nome):
+def buscar_ponto_turistico (nome):
     with open(arquivo, 'r') as f:
-        usuarios = json.load(f)
+        ponto_turisticos = json.load(f)
     
     encontrado = False
 
-    for usuario in usuarios:
-        if usuario['nome'] == nome:
-            print(f"NOME: {usuario['nome']}, IDADE: {usuario['idade']}")
+    for ponto_turistico in ponto_turisticos:
+        if ponto_turistico['nome'] == nome:
+            print(f"NOME: {ponto_turistico['nome']}, BAIRRO: {ponto_turistico['bairro']}")
             encontrado = True
     if not encontrado:
-            print("😒 NENHUM USUÁRIO CADASTRADO.")
+            print("😒 NENHUM PONTO TURISTICO CADASTRADO.")
     
 
-def linha_horizontal(cor):
-    return cor + "=" * 50 + cor['RESET']
+# def linha_horizontal(cor):
+#     return cor + "=" * 50 + cor['RESET']
 
 def menu_inicial ():
-    print (cor.CIANO + "=" *55 + cor.RESET)
-    print (cor.VERMELHO + " ---->>> BEM VINDO Ao CATÁLOGO DE PONTOS TURISTICOS DE RECIFE <<<---- ")
-    print ("          1 - MÓDULO USUÁRIO ")
+    print ("*" *55)
+    print (" ---->>>  BEM VINDO Ao CATÁLOGO DE PONTOS TURISTICOS DE RECIFE <<<---- ")
+    print ("          1 - MÓDULO PONTO TURISTICO ")
     print ("          2 - MÓDULO ESTOQUE ")
     print ("          3 - SAIR ")
-    print (cor.CIANO + "=" *55 + cor.RESET)
+    print ("*" *55)
     
 def exibir_menu():
     print("\nMENU:")
-    print("1. ADICIONAR USUÁRIO")
-    print("2. LISTAR USUÁRIOS")
-    print("3. ATUALIZAR USUÁRIO")
-    print("4. EXCLUIR USUÁRIO")
-    print("5. LISTAR UM USUÁRIO")
+    print("1. ADICIONAR PONTO TURISTICO")
+    print("2. LISTAR PONTO TURISTICOS")
+    print("3. ATUALIZAR PONTO TURISTICO")
+    print("4. EXCLUIR PONTO TURISTICO")
+    print("5. LISTAR UM PONTO TURISTICO")
     print("6. VOLTAR AO MENU ANTERIOR")
 
 
@@ -109,7 +109,7 @@ def main():
     
     while True:
         menu_inicial()
-        opcao_inicial = int(input("INFORME UMA OPÇÃO"))
+        opcao_inicial = int(input("INFORME UMA OPÇÃO: "))
 
         match (opcao_inicial):
             case 2:
@@ -118,25 +118,26 @@ def main():
             case 1:
                 while True: 
                     exibir_menu()
-                    opcao = input("ESCOLHA UMA OPÇÃO:\n>>>")
+                    opcao = input("ESCOLHA UMA OPÇÃO:\n>>> ")
 
                     if opcao == "1":
-                        nome = input(" DIGITE O NOME:\n>>>")
-                        idade = input(" DIGITE A IDADE:\n>>>")
-                        adicionar_usuario(nome, idade)
+                        nome = input(" DIGITE O NOME:\n>>> ")
+                        bairro = input(" DIGITE A BAIRRO:\n>>> ")
+                        tipo = input(" DIGITE O TIPO:\n>>> ")
+                        adicionar_ponto_turistico(nome, bairro, tipo)
                     elif opcao == "2":
-                        listar_usuarios()
+                        listar_ponto_turisticos()
                     elif opcao == "3":
-                        nome_antigo = input("DIGITE O NOME A SER ATUALIZADO:\n>>>")
-                        novo_nome = input("DIGITE O NOVO NOME:\n>>>")
-                        nova_idade = input("DIGITE A NOVA IDADE:\n>>>")
-                        atualizar_usuario(nome_antigo, novo_nome, nova_idade)
+                        nome_antigo = input("DIGITE O NOME A SER ATUALIZADO:\n>>> ")
+                        novo_nome = input("DIGITE O NOVO NOME:\n>>> ")
+                        novo_bairro = input("DIGITE A NOVA BAIRRO:\n>>> ")
+                        atualizar_ponto_turistico(nome_antigo, novo_nome, novo_bairro)
                     elif opcao == "4":
-                        nome = input("DIGITE O NOME DO USUÁRIO A SER EXCLUÍDO:\n>>>")
-                        excluir_usuario(nome)
+                        nome = input("DIGITE O NOME DO PONTO TURISTICO A SER EXCLUÍDO:\n>>> ")
+                        excluir_ponto_turistico(nome)
                     elif opcao == "5":
-                        nome = input("DIGITE O NOME DO USUÁRIO:\n>>>")
-                        buscar_usuario(nome)
+                        nome = input("DIGITE O NOME DO PONTO TURISTICO:\n>>> ")
+                        buscar_ponto_turistico(nome)
                     elif opcao == "6":
                         print("VOLTAR AO MENU ANTERIOR...")
                         sleep(3)
